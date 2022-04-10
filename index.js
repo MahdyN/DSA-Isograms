bb501230cbe13b3523b8d8211b2879208db83e82
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2022-04-10 12:01:01 -0400

    Comments adjusted

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function isIsogram(str) {
         // string is split into an array of substrings and sorted alphabetically, the sort method will make it easier to check for characters that are the same ["a", "b", "c", "d", "a"] => ["a", "a", "b", "c", "d"]
         const strArray = adjStr.split('').sort()
         
-        // this for loop begins at the 2nd element(index of 1) and checks its value against the 1st element(index of 0), it will return false if there is a duplicated character because of the sort method we previously applied which made it easier to group any characters, the loop continues for the entire length of the array
+        // this for loop begins at the 2nd element(index of 1) and checks its value against the 1st element(index of 0), it will return false if there is a duplicated character because of the sort method we previously applied which made it easier to group any repeated characters, the loop continues for the entire length of the array
         for(let i = 1; i < strArray.length; i++){
             if(strArray[i] === strArray[i - 1]){
                 return false
